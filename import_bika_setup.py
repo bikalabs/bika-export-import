7003d2dc6181d2e8ac0a75a173b8fb9c2e70d93f
Campbell <campbell@bikalabs.com>
2015-08-31 03:31:08 +0200

    Import (does not process deferred targets yet)

--- import_bika_setup.py
+++ import_bika_setup.py
@@ -8,8 +8,10 @@ import pdb
 import traceback
 import sys
 
+from Products.ATExtensions.ateapi import RecordField, RecordsField
 from Products.CMFPlone.factory import _DEFAULT_PROFILE
 from Products.CMFPlone.factory import addPloneSite
+from Products.CMFPlone.utils import _createObjectByType
 from AccessControl.SecurityManagement import newSecurityManager
 from bika.lims.catalog import getCatalog
 from Products.Archetypes import Field
@@ -19,11 +21,15 @@ import openpyxl
 
 
 def excepthook(typ, value, tb):
+    import pdb, traceback
+
     traceback.print_exception(typ, value, tb)
     pdb.pm()
+    pdb.set_trace()
 
 
 sys.excepthook = excepthook
+
 
 # If creating a new Plone site:
 default_profiles = [
@@ -33,10 +39,52 @@ default_profiles = [
     'bika.lims:default',
 ]
 
+export_types = [
+    'Client',
+    'Contact',
+    'ARPriority',
+    'AnalysisProfile',
+    'ARTemplate',
+    'AnalysisCategory',
+    'AnalysisService',
+    'AnalysisSpec',
+    'AttachmentType',
+    'BatchLabel',
+    'Calculation',
+    'Container',
+    'ContainerType',
+    'Department',
+    'Instrument',
+    'InstrumentCalibration',
+    'InstrumentCertification',
+    'InstrumentMaintenanceTask',
+    'InstrumentScheduledTask',
+    'InstrumentType',
+    'InstrumentValidation',
+    'LabContact',
+    'LabProduct',
+    'Manufacturer',
+    'Method',
+    'Preservation',
+    'ReferenceDefinition',
+    'SampleCondition',
+    'SampleMatrix',
+    'StorageLocation',
+    'SamplePoint',
+    'SampleType',
+    'SamplingDeviation',
+    'SRTemplate',
+    'SubGroup',
+    'Supplier',
+    'SupplierContact',
+    'WorksheetTemplate',
+]
+
 
 class Main:
     def __init__(self, args):
         self.args = args
+        self.deferred_targets = []
 
     def __call__(self):
         """Export entire bika site
@@ -71,17 +119,121 @@ class Main:
         # Import
         self.import_laboratory()
         self.import_bika_setup()
-        for portal_type in self.get_portal_type_sheet_names():
+        for portal_type in export_types:
             self.import_portal_type(portal_type)
         # Remove tempdir
         shutil.rmtree(self.tempdir)
 
-    def get_portal_type_sheet_names(self):
-        typestool = getToolByName(self.portal, 'portal_types')
-        for sheetname in self.wb.get_sheet_names():
-            import pdb;pdb.set_trace()
+        transaction.commit()
 
-    def mutate(self, field, value):
+    def get_catalog(self, portal_type):
+        """grab the first catalog we are indexed in
+        """
+        at = getToolByName(self.portal, 'archetype_tool')
+        return at.getCatalogsByType(portal_type)[0]
+
+    def resolve_reference_ids_to_uids(self, instance, field, value):
+        """Get target UIDs for any ReferenceField.
+        If targets do not exist, the requirement is added to deferred_targets.
+        """
+        # We make an assumption here, that if there are multiple allowed
+        # types, they will all be indexed in the same catalog.
+        target_type = field.allowed_types \
+            if isinstance(field.allowed_types, basestring) \
+            else field.allowed_types[0]
+        catalog = self.get_catalog(target_type)
+        # The ID is what is stored in the export, so first we must grab these:
+        if field.multiValued:
+            # multiValued references get their values stored in a sheet
+            # named after the relationship.
+            ids = []
+            if field.relationship[:31] not in self.wb:
+                print "%s/%s: Cannot find sheet %s." % (
+                    instance, field.relationship, field.relationship[:31])
+                return None
+            ws = self.wb[field.relationship[:31]]
+            ids = []
+            for rownr, row in enumerate(ws.rows):
+                if rownr == 0:
+                    keys = [cell.value for cell in row]
+                    continue
+                rowdict = dict(zip(keys, [cell.value for cell in row]))
+                if rowdict['Source'] == instance.id:
+                    ids.append(rowdict['Target'])
+            if not ids:
+                return []
+            final_value = []
+            for v in value:
+                brain = catalog(portal_type=field.allowed_types, id=v)
+                if brain:
+                    final_value.append(brain.getObject())
+                else:
+                    self.deferred_targets.append({
+                        'instance': instance,
+                        'field': field,
+                        'id': v
+                    })
+            return final_value
+        else:
+            brain = catalog(portal_type=field.allowed_types, id=value)
+            if brain:
+                return brain[0].getObject()
+            else:
+                self.deferred_targets.append({
+                    'instance': instance,
+                    'field': field,
+                    'id': value
+                })
+        return None
+
+    def resolve_records(self, instance, field, value):
+        # RecordField and RecordsField
+        # We must re-create the dict (or list of dicts) from sheet values
+        ws = self.wb[value]
+        matches = []
+        for rownr, row in enumerate(ws.rows):
+            if rownr == 0:
+                keys = [cell.value for cell in row]
+                continue
+            rowdict = dict(zip(keys, [cell.value for cell in row]))
+            if rowdict['id'] == instance.id \
+                    and rowdict['field'] == field.getName():
+                matches.append(rowdict)
+        if type(field.default) == dict:
+            return matches[0] if matches else {}
+        else:
+            return matches
+
+    def set(self, instance, field, value):
+        mutator = field.getMutator(instance)
+        outval = self.mutate(instance, field, value)
+        return mutator(outval)
+
+    def mutate(self, instance, field, value):
+        if type(value) in (int, bool):
+            return value
+        if isinstance(value, unicode):
+            value = value.encode('utf-8')
+        elif isinstance(field, RecordField):
+            value = self.resolve_records(instance, field, value) \
+                if value else {}
+        if isinstance(field, RecordsField) or \
+                (isinstance(value, basestring)
+                 and value
+                 and value.endswith("_values")):
+            value = self.resolve_records(instance, field, value) \
+                if value else []
+        elif Field.IReferenceField.providedBy(field):
+            value = self.resolve_reference_ids_to_uids(instance, field, value)
+        elif Field.ILinesField.providedBy(field):
+            value = value.splitlines() if value else ()
+        # TextField implements IFileField, so we must handle it before
+        # IFileField.  It does nothing to the field value.
+        elif Field.ITextField.providedBy(field):
+            pass
+        elif value and Field.IFileField.providedBy(field):
+            # XXX should not be reading entire file contents into mem.
+            value = open(value).read()
         return value
 
     def import_laboratory(self):
@@ -90,9 +242,7 @@ class Main:
         ws = self.wb['Laboratory']
         for row in ws.rows:
             field = schema[row[0].value]
-            value = self.mutate(field, row[1].value)
-            import pdb;pdb.set_trace()
-            field.set(instance, value)
+            self.set(instance, field, row[1].value)
 
     def import_bika_setup(self):
         instance = self.portal.bika_setup
@@ -100,14 +250,40 @@ class Main:
         ws = self.wb['BikaSetup']
         for row in ws.rows:
             field = schema[row[0].value]
-            value = self.mutate(field, row[1].value)
-            import pdb;pdb.set_trace()
-            field.set(instance, value)
+            self.set(instance, field, row[1].value)
 
-    def import_portal_type(self):
-        import pdb;
+    def import_portal_type(self, portal_type):
+        if portal_type not in self.wb:
+            print "No worksheet found for type %s" % portal_type
+            return None
+        pt = getToolByName(self.portal, 'portal_types')
+        if portal_type not in pt:
+            print "Error: %s not found in portal_types." % portal_type
+            return None
+        fti = pt[portal_type]
+        ws = self.wb[portal_type]
+        keys = [cell.value for cell in ws.rows[0]]
+        for rownr, row in enumerate(ws.rows[1:]):
+            rowdict = dict(zip(keys, [cell.value for cell in row]))
+            path = rowdict['path'].encode('utf-8').strip('/').split('/')
+            del (rowdict['path'])
+            uid = rowdict['uid'].encode('utf-8')
+            del (rowdict['uid'])
+            instance_id = rowdict['id'].encode('utf-8')
+            del (rowdict['id'])
+            parent = self.portal.unrestrictedTraverse(path)
 
-        pdb.set_trace()
+            # Sampletype<->SamplePoint relations are not really good things.
+            # the source setters set the back-reference directly into the
+            # target object.  That's like, four extra AT objects per relation.
+            # Here they get added to deferred_targets
+
+            instance = _createObjectByType(portal_type, parent, instance_id)
+            instance.unmarkCreationFlag()
+            instance.reindexObject()
+            for fieldname, value in rowdict.items():
+                field = instance.schema[fieldname]
+                self.set(instance, field, value)
 
 
 if __name__ == '__main__':
